c60849eb4796a9b2884120b0073ff0c1f02dc4a6
Costantin <Cosbgn@gmail.com>
2016-09-22 11:25:06 +0200

    working layouts! :D

--- review_app/views.py
+++ review_app/views.py
@@ -2,9 +2,9 @@ from flask import render_template
 from review_app import app
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!!'
+def index():
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
-    return 'login page!'
+    return render_template('login.html')
